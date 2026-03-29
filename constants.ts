7a4b2ac648a9b1f65fa13d07a8e0d9bfbfee594b
auduatiku-lab <auduatiku@gmail.com>
2026-03-29 20:22:38 +0100

    feat: Add new Angola Eurobond data
    Includes data for ANGOLA 9.125% 03/31/33 and ANGOLA 9.375% 10/15/35, enabling expanded pricing calculations.

--- constants.ts
+++ constants.ts
@@ -26,6 +26,14 @@ export const BONDS: Bond[] = [
     currency: 'USD'
   },
   {
+    id: 'AO-2035',
+    name: 'ANGOLA 9.375% 10/15/35',
+    couponRate: 9.375,
+    maturityDate: '2035-10-15',
+    frequency: 2,
+    currency: 'USD'
+  },
+  {
     id: 'NG-2027-NOV',
     name: 'NIGERIA 6.500% 11/28/27',
     couponRate: 6.5,
