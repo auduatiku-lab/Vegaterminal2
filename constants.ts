168f61560a68e4cf424fea65ae2ca0cc0a5655f5
auduatiku-lab <auduatiku@gmail.com>
2026-03-29 20:54:54 +0100

    feat: Add Angola 2049 Eurobond
    Adds a new Eurobond instrument for Angola maturing in 2049 to the instrument registry. This allows for calculations and pricing of this specific bond.

--- constants.ts
+++ constants.ts
@@ -50,6 +50,14 @@ export const BONDS: Bond[] = [
     currency: 'USD'
   },
   {
+    id: 'AO-2049',
+    name: 'ANGOLA 9.125% 11/26/49',
+    couponRate: 9.125,
+    maturityDate: '2049-11-26',
+    frequency: 2,
+    currency: 'USD'
+  },
+  {
     id: 'NG-2027-NOV',
     name: 'NIGERIA 6.500% 11/28/27',
     couponRate: 6.5,
